6083804c7b6941803cc6deb1d76c686a637ed199
Cyan <c.kuo@mail.utoronto.ca>
2014-05-29 16:11:56 -0400

    Added comments and improved code readability in various scripts. Consistency script compiles but still requires testing. Some of the results files may have changed because of testing.

--- MATLAB/E3/design_ord_GC_E3.m
+++ MATLAB/E3/design_ord_GC_E3.m
@@ -1,26 +1,30 @@
 function design_ord_GC_E3(subj_k)
+%% Stimuli ordering script
+    %{ 
+        Takes subject number as input, outputs a txt file
+        of all possible pairings of stimuli in a pseudorandom
+        order. Is read with bhv_exp_GC_E3.
+    %}
 
+% initialize random seed
 RandStream.setGlobalStream ...
     (RandStream('mt19937ar','seed',sum(100*clock)))
 
+% set defaults if no arguments provided
 if nargin==0
     subj_k=0; 
 end
 
-%run_n=2;
-
-%%%pseudorandomize stimulus order
-%%%cnd2: no image is repeated twice in a row
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%perm here so set of id pairs in a run is random
-
+% pseudorandomize stimulus order
 tuples_ordered = nchoosek(1:60, 2);
 perm_index = randperm(1770);
 
+% initialize variables for loops
 j = 1;
-num_removed = 0;
 tuples_random(j,:) = tuples_ordered(perm_index(j),:);  % (*) okay when j=1 :)
 tup_accum = [];
+
+% check condition: no image is repeated twice in a row
 for k = 2:1770
     
     %For every tuple, ensure that its entries do not appear twice 
@@ -38,28 +42,29 @@ for k = 2:1770
             tuples_random(j,:) = tuples_ordered(perm_index(k),:);  
         else
             tuples_random(j,:) = flip(tuples_ordered(perm_index(k),:));
-        end % (*) passed
+        end
         
     else
         
-        num_removed = num_removed + 1;                         % (*) failed
         % accumulate rejected tuples
         tup_accum = [tup_accum;tuples_ordered(perm_index(k),:)]; %#ok<AGROW>
         
     end
 end
 
-
-m = size(tuples_random, 1) - 1;
 % m is the index at which rejected tuples are inserted to tuples_random
+m = size(tuples_random, 1) - 1;
 
 while size(tup_accum, 1) > 0
     %{
     This if block checks for violations of the conditions
     outlined above. Obviously, this can be done far more
     parsimoniously by turning condition checking into a
-    separate script but I'm lazy. We'll leave this on a TODO
-    given the time constraints and my current mental state.
+    separate script but I'm lazy and the implementation
+    turned out messier than I thought. In the interest of
+    time and transparency to those who maintain the code,
+    I've left all this repeated stuff (with modifications)
+    here so you can play with it as needed.
     -- Cy
     %}
     if tuples_random(m,1) ~= tup_accum(1,1) && ...
@@ -93,10 +98,12 @@ while size(tup_accum, 1) > 0
     end
 end
 
+% split tuples_random into 6 runs
 ord_mat = [tuples_random(1:295,:) tuples_random(296:590,:) ...
     tuples_random(591:885,:) tuples_random(886:1180,:) ...
     tuples_random(1181:1475,:) tuples_random(1476:1770,:)];
 
+% output the resulting matrix to file
 ord_fold='stims_ord_E3/';
 fl=[ord_fold, 's', sprintf('%02.0f', subj_k), '_ord_mat.txt']; 
 dlmwrite(fl, ord_mat, 'precision', '%03.0f');
